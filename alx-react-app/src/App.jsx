d5367b722a1e804b850d544d9c5e5fc7f5963ab9
NahomZewdie <zewdienahom@gmail.com>
2025-07-13 23:52:09 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
     return (
